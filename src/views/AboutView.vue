0face8da0df5cfc9c82418e681693e66b0d19376
frasfras <65541080+frasfras@users.noreply.github.com>
2022-07-04 19:42:07 +0800

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <HelloCourse />
+  </div>
+</template>
+<script>
+import HelloCourse from "@/components/CourseView.vue";
+/* eslint-disable */ 
+export default {
+  name: "AboutView",
+  components: {
+    HelloCourse,
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Water plan" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/CoursesWorld.vue";
+/* eslint-disable */ 
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
