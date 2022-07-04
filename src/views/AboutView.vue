372fd21e8f3809889f80ee09627af07665a1a182
frasfras <65541080+frasfras@users.noreply.github.com>
2022-07-04 20:00:00 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,8 @@
 <template>
   <div class="about">
-    <HelloCourse />
+    
+     <h1>Formulate Waterplan </h1>
+    <pega-embed id='theEmbed' action='createCase' caseTypeID='MyOrg-GolfGreen-Work-Waterplan' casePage='full' appAlias='golf-green' pegaServerUrl='https://sw5fyxc4.pegace.net/prweb/' staticContentUrl='https://community.constellation.pega.io/c11n/' authService='SSO' clientId='11445058306307655452' style='width:100%'>
+</pega-embed>
   </div>
 </template>
-<script>
-import HelloCourse from "@/components/CourseView.vue";
-/* eslint-disable */ 
-export default {
-  name: "AboutView",
-  components: {
-    HelloCourse,
-  },
-};
-</script>
